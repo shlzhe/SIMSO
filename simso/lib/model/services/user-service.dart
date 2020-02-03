eaee86420743b954779ee5d303ce444a5efdc094
Caleb Robinson <calebandrewrobinson@gmail.com>
2020-02-03 17:07:31 -0600

    Added one class as well as constants

--- simso/lib/model/services/user-service.dart
+++ simso/lib/model/services/user-service.dart
@@ -4,10 +4,14 @@ import 'package:http/http.dart';
 import 'iuser-service.dart';
 import 'dart:convert';
 
+// The implimentation of IUserService.
+// Here we make the actual call to the database
 class UserService extends IUserService {
+  
+  // This is the subsection of the API we need to call
   String url = APIConstants.BaseAPIURL + '/users';
 
-
+  // Implimentation of the getUserDataByID
   @override
   Future<UserModel> getUserDataByID(String id) async {
     Response response = await get('$url/$id');
@@ -17,6 +21,7 @@ class UserService extends IUserService {
     return user;
   }
 
+  // Implimentation of the saveUser
   @override
   Future<String> saveUser(UserModel user) async {
     Response response = await post(url, body: user.toJson());
---
+++ simso/lib/view/design-constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/widgets.dart';
+
+// This class is where we store colors that will be used throughout the app
+class DesignConstants {
+  
+  static const Color exampleColorBlue = const Color(0xFF68C3D4);
+}
