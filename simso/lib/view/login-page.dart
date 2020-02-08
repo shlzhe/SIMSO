bc29ecc6a0d2ac0b090deb1101cfe73af63718e6
Leluuu85 <48428381+Leluuu85@users.noreply.github.com>
2020-02-07 21:36:04 -0600

    Login with password and email.

--- simso/lib/view/login-page.dart
+++ simso/lib/view/login-page.dart
@@ -1,7 +1,9 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:simso/controller/login-page-controller.dart';
 
 import '../model/entities/user-model.dart';
+
 class LoginPage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
@@ -13,6 +15,8 @@ class LoginPageState extends State<LoginPage> {
   BuildContext context;
   LoginPageController controller;
   UserModel user;
+  String url =
+      'https://firebasestorage.googleapis.com/v0/b/capstone-16d44.appspot.com/o/ApplicationImages%2FSimSoLogo.png?alt=media&token=9285c534-aca6-4833-ab47-40bbd8dee518';
   var formKey = GlobalKey<FormState>();
   LoginPageState() {
     controller = LoginPageController(this);
@@ -27,32 +31,46 @@ class LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     this.context = context;
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Login'),
-      ),
-      body:  Container(
-        child: Form(
-            key: formKey,
-            child: Column(children: <Widget>[
-              TextFormField(
-              decoration: InputDecoration(
-                labelText: 'Enter email as login name',
-                hintText: 'email',
-              ),
-              keyboardType: TextInputType.emailAddress,
-              validator: controller.validateEmail,
-              onSaved: controller.saveEmail,
-              ),
-              RaisedButton(
-                onPressed: controller.goToHomepage,
-                child: Text(
-                  'Dont enter email. Go to Homepage',
+      // appBar: AppBar(
+      //   title: Text('Login'),
+      // ),
+      body: Container(
+          child: Form(
+        key: formKey,
+        child: ListView(
+          children: <Widget>[
+            Column(
+              children: <Widget>[
+                CachedNetworkImage(imageUrl: url),
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'Enter email as login name: a@uco.edu',
+                    hintText: 'email',
+                  ),
+                  keyboardType: TextInputType.emailAddress,
+                  validator: controller.validateEmail,
+                  onSaved: controller.saveEmail,
                 ),
-              ),
-            ],
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'Enter password: 123456',
+                    hintText: 'password',
+                  ),
+                  keyboardType: TextInputType.visiblePassword,
+                  validator: controller.validatePassword,
+                  onSaved: controller.savePassword,
+                ),
+                RaisedButton(
+                  onPressed: controller.goToHomepage,
+                  child: Text(
+                    'Login',
+                  ),
+                ),
+              ],
             ),
-        )
-      ),
+          ],
+        ),
+      )),
     );
   }
 }
