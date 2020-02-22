433a203e26d03e016248255b2cbbccb59b35a67e
Fiona Diep Nguyen <56050215+FionaDN87@users.noreply.github.com>
2020-02-21 22:53:56 -0600

    Complete SignOut button

--- simso/lib/view/navigation-drawer.dart
+++ simso/lib/view/navigation-drawer.dart
@@ -1,8 +1,10 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:simso/model/entities/user-model.dart';
 import 'package:simso/view/homepage.dart';
+import 'package:simso/view/login-page.dart';
 
 import 'design-constants.dart';
 
@@ -19,7 +21,44 @@ class MyDrawer extends StatelessWidget {
   }
 
   void signOut(){
+    //print('${state.user.email}');
     
+    FirebaseAuth.instance.signOut();    //Email/pass sign out
+    //state.googleSignIn.signOut();       //Goole sign out
+     //Display confirmation dialog box after user clicking on "Sign Out" button
+    showDialog (
+      context: context,
+      builder: (BuildContext context){
+        return AlertDialog(
+          title: Text('Confirmation',style: TextStyle(color: DesignConstants.yellow,fontSize: 30),),
+          content: Text('Would you like to sign out?', style: TextStyle(color: DesignConstants.yellow)) ,
+          backgroundColor: DesignConstants.blue,
+          actions: <Widget>[
+            RaisedButton(
+              child: Text('YES', style: TextStyle(color: DesignConstants.yellow,fontSize: 20),),
+              color: DesignConstants.blue,
+              onPressed: (){
+                //Dialog box pop up to confirm signing out
+                FirebaseAuth.instance.signOut();     
+                //Close Drawer, then go back to Front Page
+                Navigator.pop(context);  //Close Dialog box
+                Navigator.pop(context);  //Close Drawer
+                //Navigator.pop(state.context);  //Close Home Page 
+                Navigator.push(context, MaterialPageRoute(
+                  builder: (context)=> LoginPage(),
+                ));
+              },
+            ),
+            RaisedButton(
+              child: Text('NO', style: TextStyle(color: DesignConstants.yellow, fontSize: 20),),
+              color: DesignConstants.blue,
+              onPressed: ()=>Navigator.pop(context),  //close dialog box 
+            ),
+          ],
+        );
+        
+      },
+    );
   }
 
 
