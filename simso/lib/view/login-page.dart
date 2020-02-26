a0b8febf3687080858669cca5ac77745ec7a6b18
Fiona Diep Nguyen <56050215+FionaDN87@users.noreply.github.com>
2020-02-26 12:07:56 -0600

    Added Google Sign In logo and button on Login Page

--- simso/lib/view/login-page.dart
+++ simso/lib/view/login-page.dart
@@ -124,6 +124,31 @@ class LoginPageState extends State<LoginPage> {
                                 textColor: DesignConstants.yellow,
                                 color: DesignConstants.blueLight,
                               ),
+                              //----------------------------------------------------
+                          //GOOGLE SIGN IN BUTTON
+                          OutlineButton(
+                            onPressed: controller.googleSignIn,
+                            splashColor: Colors.grey,
+                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(40)),
+                            highlightElevation: 0,
+                            borderSide: BorderSide(color: DesignConstants.yellow),
+                            child: Padding(padding:const EdgeInsets.fromLTRB(0, 10, 0, 10), 
+                              child: Row(
+                                      mainAxisSize: MainAxisSize.min,
+                                      mainAxisAlignment: MainAxisAlignment.center,
+                                      children: <Widget>[
+                                        Image(image:AssetImage("assets/images/google_logo.png"), height: 35.0),
+                                        Padding(
+                                          padding: const EdgeInsets.only(left: 10),
+                                          child: Text('Sign In with Google Account',style:TextStyle(color:DesignConstants.yellow,fontSize: 15),
+                                          )
+                                        )
+                                      ],
+
+                              )
+                            ),
+                            ) 
+                            //----------------------------------------------------
                       ],
                     ),
                   ],
