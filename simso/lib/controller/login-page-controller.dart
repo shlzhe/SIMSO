297201ec051b311e8c457429a8a9a04863ed6b9a
Fiona Diep Nguyen <56050215+FionaDN87@users.noreply.github.com>
2020-02-26 12:05:38 -0600

    Added functions for Google Sign In

--- simso/lib/controller/login-page-controller.dart
+++ simso/lib/controller/login-page-controller.dart
@@ -1,5 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:simso/model/services/iuser-service.dart';
 import 'package:simso/view/create-account.dart';
 import 'package:simso/view/homepage.dart';
