1a445161d4e8d4e05a47df869ffc6ba9f9537f0a
Fiona Diep Nguyen <56050215+FionaDN87@users.noreply.github.com>
2020-02-27 16:37:18 -0600

    Fixed bug

--- simso/lib/model/entities/globals.dart
+++ simso/lib/model/entities/globals.dart
@@ -2,7 +2,6 @@
 library simso.globals;
 
 import 'package:simso/model/entities/timer-model.dart';
-import 'package:simso/model/entities/touch-counter-model.dart';
 
 TimerModel timer;
 TouchCounterModel touchCounter;
