69f36a0659286342a9ac1f42f1140898af90c134
Caleb Robinson <calebandrewrobinson@gmail.com>
2020-02-28 15:50:26 -0600

    Fixing merge conflicts

--- simso/lib/view/navigation-drawer.dart
+++ simso/lib/view/navigation-drawer.dart
@@ -33,17 +33,63 @@ class MyDrawer extends StatelessWidget {
         context, MaterialPageRoute(builder: (context) => SnapshotPage()));
   }
 
-    void navigateMemePage() {
+  void navigateMemePage() {
     Navigator.push(
         context, MaterialPageRoute(builder: (context) => MemePage()));
   }
 
-      void navigateAccountSettingPage() {
+  void navigateAccountSettingPage() {
     Navigator.push(
         context, MaterialPageRoute(builder: (context) => AccountSettingPage()));
   }
 
-=======
+  void signOut(){
+    //print('${state.user.email}');
+    
+    FirebaseAuth.instance.signOut();    //Email/pass sign out
+    //state.googleSignIn.signOut();       //Goole sign out
+     //Display confirmation dialog box after user clicking on "Sign Out" button
+    showDialog (
+      context: context,
+      builder: (BuildContext context){
+        return AlertDialog(
+          title: Text('Confirmation',style: TextStyle(color: DesignConstants.yellow,fontSize: 30),),
+          content: Text('Would you like to sign out?', style: TextStyle(color: DesignConstants.yellow)) ,
+          backgroundColor: DesignConstants.blue,
+          actions: <Widget>[
+            RaisedButton(
+              child: Text('YES', style: TextStyle(color: DesignConstants.yellow,fontSize: 20),),
+              color: DesignConstants.blue,
+              onPressed: (){
+                //Dialog box pop up to confirm signing out
+                FirebaseAuth.instance.signOut();     
+                //Close Drawer, then go back to Front Page
+                Navigator.pop(context);  //Close Dialog box
+                Navigator.pop(context);  //Close Drawer
+                //Navigator.pop(state.context);  //Close Home Page 
+                Navigator.push(context, MaterialPageRoute(
+                  builder: (context)=> LoginPage(),
+                ));
+              },
+            ),
+            RaisedButton(
+              child: Text('NO', style: TextStyle(color: DesignConstants.yellow, fontSize: 20),),
+              color: DesignConstants.blue,
+              onPressed: ()=>Navigator.pop(context),  //close dialog box 
+            ),
+          ],
+        );
+        
+      },
+    );
+  }
+
+  void navigateTimeManagement() async {
+    Navigator.push(context, MaterialPageRoute(
+      builder: (context) => TimeManagementPage(user)
+    ));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Drawer(
@@ -66,42 +112,33 @@ class MyDrawer extends StatelessWidget {
             accountName: Text(user.username),
             accountEmail: Text(user.email),
           ),
-
-          Card(
-            child: ListTile(
-              leading: Icon(Icons.bubble_chart),
-              title: Text('My Thoughts'),
-              onTap: navigateHomepage,
-            ),
+          ListTile(
+            leading: Icon(Icons.home),
+            title: Text('Homepage'),
+            onTap: navigateHomepage,
           ),
-          Card(
-            child: ListTile(
-              leading: Icon(Icons.group),
-              title: Text('Friends'),
-              onTap: navigateHomepage,
-            ),
+          ListTile(
+            leading: Icon(Icons.timer),
+            title: Text('Time Management'),
+            onTap: navigateTimeManagement,
           ),
-          Card(
-            child: ListTile(
+          ListTile(
               leading: Icon(Icons.group_add),
               title: Text('Recommended Friends'),
               onTap: controller.recommendFriends,
-            ),
-
-          ),
-          
+            ),    
           ListTile(
-            leading: Icon(Icons.bubble_chart),
+            leading: Icon(Icons.camera),
             title: Text('My Snapshots'),
             onTap: navigateSnapshotPage,
           ), 
                     ListTile(
-            leading: Icon(Icons.bubble_chart),
+            leading: Icon(Icons.mood),
             title: Text('My Memes'),
             onTap: navigateMemePage,
           ), 
                     ListTile(
-            leading: Icon(Icons.bubble_chart),
+            leading: Icon(Icons.settings),
             title: Text('Account Settings'),
             onTap: navigateAccountSettingPage,
           ), 
@@ -109,7 +146,7 @@ class MyDrawer extends StatelessWidget {
            ListTile(
             leading: Icon(Icons.exit_to_app),
                 title: Text('Sign Out'),
-                onTap: null,
+                onTap: signOut,
               ),    //Special Widget for Drawer
         ],
       ),
