3d28e0b57ac9acfd441a9f51ab5885ed08e65181
Fiona Diep Nguyen <56050215+FionaDN87@users.noreply.github.com>
2020-03-02 16:10:06 -0600

    Working on userList

--- simso/lib/controller/mainChatPage-controller.dart
+++ simso/lib/controller/mainChatPage-controller.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:simso/model/entities/user-model.dart';
 import 'package:simso/model/services/itimer-service.dart';
 import 'package:simso/model/services/itouch-service.dart';
@@ -7,9 +8,30 @@ class MainChatPageController {
   MainChatPageState state;
   ITimerService timerService;
   ITouchService touchService;
-  UserModel newUser = UserModel();
+  UserModel user;
+  List<UserModel>userList;
+
   String userID;
   //Constructor
-  MainChatPageController (this.state, this.timerService, this.touchService);
+  MainChatPageController (this.state, this.timerService, this.touchService,this.userList);
 
+
+  Future<void> showUsers() async {
+    print('showUsers() called');
+        try{
+            QuerySnapshot querySnapshot = await Firestore.instance.collection('users').getDocuments();
+            if(querySnapshot == null || querySnapshot.documentChanges.length==0){
+            print('Nothing in users collection');
+           }
+    
+    for(DocumentSnapshot doc in querySnapshot.documents){
+      print('${doc.documentID}');
+      //userList.add(UserModel.deserialize(doc.data))
+      userList.add(UserModel.deserialize(doc.data));
+    }
+    }catch(e){
+      throw e.toString();
+    }
+    
+}
 }
--- simso/lib/view/mainChat-page.dart
+++ simso/lib/view/mainChat-page.dart
@@ -1,20 +1,24 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:simso/controller/mainChatPage-controller.dart';
+import 'package:simso/model/services/itouch-service.dart';
+import 'package:simso/view/navigation-drawer.dart';
+import 'package:unicorndial/unicorndial.dart';
 import 'package:flutter/material.dart';
 import 'package:simso/controller/homepage-controller.dart';
-import 'package:simso/model/entities/user-model.dart';
 import 'package:simso/model/services/itimer-service.dart';
-import 'package:simso/model/services/itouch-service.dart';
 import 'package:simso/model/services/iuser-service.dart';
-
+import '../model/entities/user-model.dart';
 import '../service-locator.dart';
+import 'design-constants.dart';
 
 class MainChatPage extends StatefulWidget {
   final UserModel user;
 
- MainChatPage(this.user);
+  MainChatPage(this.user);
 
   @override
   State<StatefulWidget> createState() {
-    return MainChatPageState (user);
+    return MainChatPageState(user);
   }
 }
 
@@ -25,23 +29,48 @@ class MainChatPageState extends State<MainChatPage> {
   ITouchService touchService = locator<ITouchService>();
   MainChatPageController controller;
   UserModel user;
+  List<UserModel>userList;                                //To hold all SimSO users, initialized null
   String returnedID;
   var idController = TextEditingController();
   var formKey = GlobalKey<FormState>();
-
+  
   MainChatPageState(this.user) {
-    controller = MainChatPage(this, this.timerService, this.touchService);
-    controller.setupTimer();
-    controller.setupTouchCounter();
+    controller = MainChatPageController(this, this.timerService, this.touchService,this.userList);
+  
   }
 
   void stateChanged(Function f) {
     setState(f);
   }
+
   @override
   Widget build(BuildContext context) {
-  
-    return null;
-  }
+    this.context = context;
+    return Scaffold(
+    
+      appBar: AppBar(
+        title: Text('SimSo Together'),
+        backgroundColor: DesignConstants.blue,
+      ),
+      drawer: MyDrawer(context, user),
+      body: new Center(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),   //Top center raise buttin
+          child: Column(     
+          children: <Widget>[  
+             new RaisedButton.icon(    
+             icon: Icon(Icons.public), 
+            label: Text('Public'),
+            textColor: DesignConstants.blue,
+            onPressed: controller.showUsers, 
+             ),  
+          ],
+      ),
+        ),
+      )
+     
+     
+      
+    );
 }
-  
+}
