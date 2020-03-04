a8d43dff0493e22e9d9419f02f157b0d386e4804
Fiona Diep Nguyen <56050215+FionaDN87@users.noreply.github.com>
2020-03-03 22:50:26 -0600

    Fixing conflicts

--- simso/lib/view/design-constants.dart
+++ simso/lib/view/design-constants.dart
@@ -10,7 +10,7 @@ class DesignConstants {
   static const Color red = const Color(0xFFD65353);
   static const String logo = 'https://firebasestorage.googleapis.com/v0/b/capstone-16d44.appspot.com/o/ApplicationImages%2FSimSoLogo.png?alt=media&token=801abda0-f288-4e32-88e8-4c835fdba93f';
   static const String profile = 'https://firebasestorage.googleapis.com/v0/b/capstone-16d44.appspot.com/o/ApplicationImages%2FProfileBlank.png?alt=media&token=5986a7c6-82eb-4ac1-9d76-a375393f2501';
-  static const String  google_logo = 'https://firebasestorage.googleapis.com/v0/b/capstone-16d44.appspot.com/o/ApplicationImages%2Fgoogle_logo.png?alt=media&token=530ee070-9a1b-4597-99ea-e4746bafb03a';
+  //static const String  google_logo = 'https://firebasestorage.googleapis.com/v0/b/capstone-16d44.appspot.com/o/ApplicationImages%2Fgoogle_logo.png?alt=media&token=530ee070-9a1b-4597-99ea-e4746bafb03a';
   static const String default_MessengerProfile = 'https://firebasestorage.googleapis.com/v0/b/capstone-16d44.appspot.com/o/ApplicationImages%2FdefaultProfile.png?alt=media&token=f9eb3ade-ad5a-44d7-b56e-83edca151c97';
 
 }
