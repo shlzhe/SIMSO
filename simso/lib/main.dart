4e9b105eea96d6da72f4e720c80db7aa3e28b5f1
Leluuu85 <48428381+Leluuu85@users.noreply.github.com>
2020-03-04 18:50:23 -0600

    Null future values in main

--- simso/lib/main.dart
+++ simso/lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:path_provider/path_provider.dart';
 import 'package:simso/model/entities/local-user.dart';
 import 'package:simso/service-locator.dart';
 import './view/login-page.dart';
@@ -6,11 +9,25 @@ import 'package:flutter/material.dart';
 import 'controller/lifecycle_manager.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  var localUser;
+  //readLocalUser reads in files from android;
+  // readLocalUser(localUser).then((value) => localUser = value);
   setupServiceLocator();
-  runApp(SimsoApp());
+  //need to get non-null values from readLocalUser();
+  print(localUser);
+  runApp(SimsoApp(localUser));
 }
+Future<String> readLocalUser(localUser)async{
+  var directory = await getApplicationDocumentsDirectory();
+  var path = directory.path;
+  var contents;
+  contents = File('$path/user.txt').readAsString();
+  return contents;
+  }
 class SimsoApp extends StatelessWidget {
-  SimsoApp();
+  final String localUser;
+  SimsoApp(this.localUser);
   @override
   Widget build(BuildContext context) {
     return LifeCycleManager(
