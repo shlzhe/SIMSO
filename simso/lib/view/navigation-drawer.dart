f9f245e9e3eeb031153b6b02e2bceb5b0cb98bac
Shi Zhe Ting <sting7@uco.edu>
2020-03-10 21:20:14 -0500

    added profile page

--- simso/lib/view/navigation-drawer.dart
+++ simso/lib/view/navigation-drawer.dart
@@ -27,6 +27,7 @@ import '../view/design-constants.dart';
 import '../view/snapshot-page.dart';
 import '../view/meme-page.dart';
 import '../view/account-setting-page.dart';
+import '../view/profile-page.dart';
 //controller import
 import '../controller/homepage-controller.dart';
 
@@ -42,6 +43,12 @@ class MyDrawer extends StatelessWidget {
   void navigateHomepage() {
     Navigator.push(context, MaterialPageRoute(
       builder: (context) => Homepage(user)
+    ));
+  }
+
+    void navigateProfile() {
+    Navigator.push(context, MaterialPageRoute(
+      builder: (context) => ProfilePage(user)
     ));
   }
 
@@ -177,6 +184,11 @@ class MyDrawer extends StatelessWidget {
             leading: Icon(Icons.home),
             title: Text('Homepage'),
             onTap: navigateHomepage,
+          ),
+          ListTile(
+            leading: Icon(Icons.person),
+            title: Text('Profile'),
+            onTap: navigateProfile,
           ),
           ListTile(
             leading: Icon(Icons.timer),
