f381c466647c56e995304aa86cde9e620f3d55b7
Mohammad Abdul Baset <epbd.abir@gmail.com>
2020-03-11 19:58:20 -0500

    GPS added

--- simso/lib/view/google-map.dart
+++ simso/lib/view/google-map.dart
@@ -1,38 +1,61 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class ViewGoogleMap  extends StatefulWidget {
+class ViewGoogleMap extends StatefulWidget {
   @override
   State<ViewGoogleMap> createState() => _GoogleMapState();
 }
 
 class _GoogleMapState extends State<ViewGoogleMap> {
-
   GoogleMapController mapController;
-  final LatLng _center = const LatLng(45.521563, -122.677433);
 
-  void _onMapCreated(GoogleMapController controller) {
-    mapController = controller;
+  LatLng _center;
+
+  _onMapCreated(GoogleMapController controller) {
+    setState(() {
+      mapController = (controller);
+    });
   }
-  
+
+  void _getGps() async {
+    Position position = await Geolocator()
+        .getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+    setState(() {
+      _center = new LatLng(position.latitude, position.longitude);
+    });
+    print("Lat: " + position.latitude.toString());
+    print("Lon: " + position.longitude.toString());
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _getGps();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          leading: IconButton(icon: Icon(Icons.arrow_back), onPressed: ()=>{
-            Navigator.pop(context)
-          }),
+          leading: IconButton(
+              icon: Icon(Icons.arrow_back),
+              onPressed: () => {Navigator.pop(context)}),
           title: Text('Maps Sample App'),
           backgroundColor: Colors.green[700],
         ),
-        body: GoogleMap(
-          onMapCreated: _onMapCreated,
-          initialCameraPosition: CameraPosition(
-            target: _center,
-            zoom: 11.0,
-          ),
-        ),
+        body: _center == null
+            ? Center(child: CircularProgressIndicator())
+            : GoogleMap(
+                onMapCreated: _onMapCreated,
+                initialCameraPosition: CameraPosition(
+                  target: _center,
+                  zoom: 8.0,
+                ),
+              ),
       ),
     );
   }
